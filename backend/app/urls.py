c8c77ae9e7fc0bb0ee5c7cc6497267fd8fcee265
0xpeIpeI <hokumaru12@gmail.com>
2022-10-14 23:49:43 +0900

    add: user api

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from rest_api.views import UserViewSet
+
+defaultRouter = routers.DefaultRouter()
+
+# api一覧
+defaultRouter.register('users',UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/',include(defaultRouter.urls)),
 ]
