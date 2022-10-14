62a4b6c030817849d1d8eec2a587d260df4484c8
0xpeIpeI <hokumaru12@gmail.com>
2022-10-15 00:56:26 +0900

    add: テーブル作成完了((仮)

--- backend/rest_api/models.py
+++ backend/rest_api/models.py
@@ -1,6 +1,6 @@
 from unicodedata import category, name
 from django.db import models
-
+from django.utils import timezone
 # ユーザ情報
 class User(models.Model):
     user_id =  models.AutoField(verbose_name="ユーザID",primary_key=True)
@@ -15,7 +15,18 @@ class Community(models.Model):
     community_name = models.CharField(verbose_name="Community Name",max_length=255)
 
 # connect users and community table
-class Member(models.Model):
+class User_Community(models.Model):
     member_id = models.AutoField(verbose_name="Member ID",primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     community = models.ForeignKey(Community, on_delete=models.CASCADE)
+
+class Article(models.Model):
+    article_id = models.AutoField(verbose_name="ID",primary_key=True)
+    article_name = models.CharField(verbose_name="投稿タイトル",max_length=255)
+    article_content = models.TextField(verbose_name="投稿内容",max_length=255)
+    meeting_time = models.DateTimeField(verbose_name="開催日時", default=timezone.now())
+    created_at = models.DateTimeField(verbose_name="投稿作成日時", default=timezone.now())
+
+class Article_Community(models.Model):
+    article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
+    community_id = models.ForeignKey(Community, on_delete=models.CASCADE)
