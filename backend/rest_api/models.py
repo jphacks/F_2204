02b85cf52546906ad846b55ae8931e7fa1a2d275
0xpeIpeI <hokumaru12@gmail.com>
2022-10-14 12:16:03 +0900

    add: Userモデル作成

--- backend/rest_api/models.py
+++ backend/rest_api/models.py
@@ -1,3 +1,9 @@
+from unicodedata import category, name
 from django.db import models
 
-# Create your models here.
+# ユーザ情報
+class User(models.Model):
+    user_id =  models.AutoField(verbose_name="ユーザID",primary_key=True)
+    user_name = models.CharField(verbose_name="ユーザ名",max_length=255)
+    address = models.CharField(verbose_name="住所",max_length=255)
+    created_at = models.DateTimeField(verbose_name='作成日時',auto_now_add=True) 
