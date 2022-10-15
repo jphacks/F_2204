d2328c28b7c9e612a05925593f06a4ead9eea643
kengo78 <fulken35@gmail.com>
2022-10-15 14:57:06 +0900

    モデルimport忘れ修正

--- backend/rest_api/views.py
+++ backend/rest_api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from .models import User, Community, User_Community, Article
+from .models import User, Community, User_Community, Article, Article_Community
 from .serializer import UserSerializer, CommunitySerializer, User_CommunitySerializer,ArticleSerializer, Article_CommunitySerialzer
 
 
