01a9c97c97a4a169cfdf0d2a4dba12d349e72ce1
0xpeIpeI <hokumaru12@gmail.com>
2022-10-18 23:27:29 +0900

    add: communityが持つuserを取得するapi

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('api/users/<int:pk>/', UserDetail.as_view()),
     path('api/communities/', CommunityList.as_view()),
     path('api/communities/<int:pk>/', CommunityDetail.as_view()),
+    path('api/communities/<int:pk>/users', UsersOfCommunities.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
