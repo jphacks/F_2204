6c640c1b2449f5d4cfe40790d6745dcec246c5da
0xpeIpeI <hokumaru12@gmail.com>
2022-10-19 01:15:08 +0900

    add: userとcommunityのview作成

--- backend/rest_api/views.py
+++ backend/rest_api/views.py
@@ -1,11 +1,25 @@
 from rest_framework import viewsets
 from .models import Community, User
-from .serializer import CommunitySerializer, UserSerializer
+from .serializer import CommunityMembersSerializer, CommunitySerializer, UserSerializer
 from rest_framework.views import APIView
 from rest_framework import status
 from django.http import Http404
 from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 
+class CommunityMembersAPIView(APIView):
+    authentication_classes = []
+    permission_classes = [AllowAny, ]
+
+    def get(self, request, pk, *args, **kwargs):
+        communities = get_object_or_404(Community, pk=pk)
+        serializer = CommunityMembersSerializer(communities)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
 
 
 class UserList(APIView):
