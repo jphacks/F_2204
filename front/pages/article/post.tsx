97f7a8df306943cfed3777af960e15c8b6557a87
Yuta-NoobCoder <ukiyutam4a1@gmail.com>
2022-10-20 13:22:41 +0900

    命名変更 create -> post

--- front/pages/article/post.tsx
+++ front/pages/article/post.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from "next";
 import BaseLayout from "../../components/templates/BaseLayout";
 
-const ChatSpacePage: NextPage = () => {
+const PostPage: NextPage = () => {
   return <BaseLayout>/article/create</BaseLayout>;
 };
 
-export default ChatSpacePage;
+export default PostPage;
