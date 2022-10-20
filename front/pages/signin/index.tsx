ad840e2b07f2ffe3b0bfb0ca2e14ea7f8afbf3fa
Yuta Uki <ukiyutaM4A1@gmail.com>
2022-10-20 12:36:18 +0900

    add signin page

---
+++ front/pages/signin/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import BaseLayout from "../../components/templates/BaseLayout";
+
+const SigninPage: NextPage = () => {
+  return <BaseLayout>/signin</BaseLayout>;
+};
+
+export default SigninPage;
---
+++ front/pages/signup/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import BaseLayout from "../../components/templates/BaseLayout";
+
+const SignupPage: NextPage = () => {
+  return <BaseLayout>/signup</BaseLayout>;
+};
+
+export default SignupPage;
