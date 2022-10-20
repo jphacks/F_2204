b8ee965664cd632d1a0c60e5adb9caddbcba7aa3
kengo78 <fulken35@gmail.com>
2022-10-21 05:02:20 +0900

    adminサイトへのモデルの追加

--- backend/rest_api/admin.py
+++ backend/rest_api/admin.py
@@ -1,6 +1,10 @@
 
 # Register your models here.
 from django.contrib import admin
-from .models import User
+from .models import *
 
 admin.site.register(User)
+admin.site.register(Community)
+admin.site.register(CommunityMembers)
+admin.site.register(Article)
+admin.site.register(Article_Community)
