28575a8f4b6c9d89735a537c39cde783dfb2b0cf
0xpeIpeI <hokumaru12@gmail.com>
2022-10-21 21:20:09 +0900

    modify: トークン時間の変更

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -65,8 +65,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    #トークンの時間を5分に設定
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    #トークンの時間を120分に設定
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
