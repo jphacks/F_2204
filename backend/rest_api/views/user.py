f0a3acae0d5544b222fb204ca41944e1b1e14866
0xpeIpeI <hokumaru12@gmail.com>
2022-10-21 21:09:52 +0900

    add: user登録機能の絞り込み.AuthReigsterのいミニした

--- backend/rest_api/views/user.py
+++ backend/rest_api/views/user.py
@@ -7,6 +7,10 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import authentication, permissions, generics
 from django.db import transaction
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 class AuthRegister(generics.CreateAPIView):
     permission_classes = (permissions.AllowAny,)
@@ -28,12 +32,12 @@ class UserList(APIView):
         selializer = UserSerializer(users, many=True)
         return Response(selializer.data)
     
-    def post(self, request, format=None):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def post(self, request, format=None):
+    #     serializer = UserSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserDetail(APIView):
