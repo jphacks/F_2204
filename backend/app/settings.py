de7fe9a68a2e51ca3e4eb0ba61281a84fc9fadc1
fulken <fulken35@gmail.com>
2022-10-21 23:21:45 +0900

    インフラ外部公開用

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -16,17 +16,17 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
+SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SECURE = True
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get('DEBUG', default=0))
-
-ALLOWED_HOSTS = ["*"]
+#DEBUG = int(os.environ.get('DEBUG', default=0))
+DEBUG=True
+ALLOWED_HOSTS = ["54.64.76.116","127.0.0.1","ec2-54-64-76-116.ap-northeast-1.compute.amazonaws.com"]
 
 
 # Application definition
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
